e3326872cdf48936250b109f345fcd460b5dc4d5
boundsj <jesse@rebounds.net>
2018-04-25 22:12:48 -0700

    Remove sqlite based model controller example

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -10,13 +10,13 @@ public func configure(
     _ env: inout Environment,
     _ services: inout Services
 ) throws {
-    /// Register providers first
-    try services.register(FluentSQLiteProvider())
-
     /// Register routes to the router
     let router = EngineRouter.default()
     try routes(router)
     services.register(router, as: Router.self)
+
+    // Register Leaf templating
+    try services.register(LeafProvider())
 
     /// Register middleware
     var middlewares = MiddlewareConfig() // Create _empty_ middleware config
@@ -24,28 +24,4 @@ public func configure(
     middlewares.use(DateMiddleware.self) // Adds `Date` header to responses
     middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
     services.register(middlewares)
-
-    // Configure a SQLite database
-    let sqlite: SQLiteDatabase
-    if env.isRelease {
-        /// Create file-based SQLite db using $SQLITE_PATH from process env
-        sqlite = try SQLiteDatabase(storage: .file(path: Environment.get("SQLITE_PATH")!))
-    } else {
-        /// Create an in-memory SQLite database
-        sqlite = try SQLiteDatabase(storage: .memory)
-    }
-
-    // Register Leaf templating
-    try services.register(LeafProvider())
-
-    /// Register the configured SQLite database to the database config.
-    var databases = DatabaseConfig()
-    databases.add(database: sqlite, as: .sqlite)
-    services.register(databases)
-
-    /// Configure migrations
-    var migrations = MigrationConfig()
-    migrations.add(model: Todo.self, database: .sqlite)
-    services.register(migrations)
-
 }
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -9,12 +9,6 @@ public func routes(_ router: Router) throws {
     router.get { req -> Future<View> in
         let leaf = try req.make(LeafRenderer.self)
         let context = [String: String]()
-        return try leaf.render("index", context)
+        return leaf.render("index", context)
     }
-
-    // Example of configuring a controller
-    let todoController = TodoController()
-    router.get("todos", use: todoController.index)
-    router.post("todos", use: todoController.create)
-    router.delete("todos", Todo.parameter, use: todoController.delete)
 }
