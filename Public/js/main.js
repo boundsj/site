64ef8b8ba117f57e919310af0c2f96b674058531
boundsj <jesse@rebounds.net>
2018-06-10 21:30:10 -0700

    Add simple dark mode

---
+++ Public/js/main.js
@@ -0,0 +1,10 @@
+$('#mode').change(function(){
+    if ($(this).prop('checked'))
+    {
+        $('body').addClass('dark-mode');
+    }
+    else
+    {
+        $('body').removeClass('dark-mode');
+    } 
+});
