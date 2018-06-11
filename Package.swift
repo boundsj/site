27895da6438fd79bbb3e48b5048c0e47c967a178
boundsj <jesse@rebounds.net>
2018-06-10 21:14:50 -0700

    Update dependencies
    Update to Vapor 3+ release

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "site",
     dependencies: [
-        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0-rc.2"),
+        .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
         .package(url: "https://github.com/vapor/fluent-sqlite.git", from: "3.0.0-rc.2"),
         .package(url: "https://github.com/vapor/leaf.git", from: "3.0.0-rc.2.1.2"),
     ],
