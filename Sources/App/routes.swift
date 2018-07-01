459e01d19ed8e3526488d24d6b442cc59f8bd21c
boundsj <jesse@rebounds.net>
2018-07-01 07:53:41 -0700

    Make working directory utility local to routes for now

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -16,10 +16,40 @@ public func routes(_ router: Router) throws {
 
     router.get("posts") { req -> Future<View> in
         let leaf = try req.make(LeafRenderer.self)
-        let dir = FileManager.workingDir()
+        let dir = workingDir()
         let location = NSString(string: "\(dir)/Posts/test.md").expandingTildeInPath
         let fileContent = try! String(contentsOfFile: location)
         let context = ["md": try markdownToHTML(fileContent)]
         return leaf.render("md", context)
     }
+}
+
+public func workingDir() -> String {
+    let fileBasedWorkDir: String?
+
+    #if Xcode
+    // attempt to find working directory through #file
+    let file = #file
+    fileBasedWorkDir = file.components(separatedBy: "/Sources").first
+    #else
+    fileBasedWorkDir = nil
+    #endif
+
+    let workDir: String
+    if let fileBasedWorkDir = fileBasedWorkDir {
+        workDir = fileBasedWorkDir
+    } else {
+        // get actual working directory
+        let cwd = getcwd(nil, Int(PATH_MAX))
+        defer {
+            free(cwd)
+        }
+        if let cwd = cwd, let string = String(validatingUTF8: cwd) {
+            workDir = string
+        } else {
+            workDir = "./"
+        }
+    }
+
+    return workDir
 }
