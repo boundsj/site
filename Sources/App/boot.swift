d1a6b9eae42e11585320c3aa2fcaac1a371169a0
boundsj <jesse@rebounds.net>
2018-07-04 08:18:01 -0700

    Use dashes for post link

--- Sources/App/boot.swift
+++ Sources/App/boot.swift
@@ -1,7 +1,9 @@
 import Routing
 import Vapor
 
-var allPosts = [String]()
+var allPostsDashed = [String]()
+var allPostsSpaced = [String]()
+var allPostsZipped = [(String, String)]()
 var postsByDate = [String: String]()
 var postPathByTitle = [String: String]()
 
@@ -9,7 +11,7 @@ var postPathByTitle = [String: String]()
 ///
 /// [Learn More →](https://docs.vapor.codes/3.0/getting-started/structure/#bootswift)
 public func boot(_ app: Application) throws {
-    if allPosts.count == 0 {
+    if allPostsDashed.count == 0 {
         let dir = workingDir()
         let path = "\(dir)/Posts/"
         let enumerator = FileManager.default.enumerator(atPath: path)
@@ -18,10 +20,13 @@ public func boot(_ app: Application) throws {
             let elementWithNoDateString = element.replacingOccurrences(of: "\(dateString)_", with: "")
             let elementWithNoExtension = elementWithNoDateString.replacingOccurrences(of: ".md", with: "")
             let components = elementWithNoExtension.components(separatedBy: "_")
-            let postTitle = components.joined(separator: " ")
-            allPosts.append(postTitle)
-            postsByDate[dateString] = postTitle
-            postPathByTitle[postTitle] = element
+            let postTitleSpaced = components.joined(separator: " ")
+            let postTitleDashed = components.joined(separator: "-")
+            allPostsDashed.append(postTitleDashed)
+            allPostsSpaced.append(postTitleSpaced)
+            postsByDate[dateString] = postTitleDashed
+            postPathByTitle[postTitleDashed] = element
         }
+        allPostsZipped = Array(zip(allPostsSpaced, allPostsDashed))
     }
 }
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -3,6 +3,11 @@ import Vapor
 import Leaf
 import SwiftMarkdown
 
+struct Post: Codable {
+    var title: String
+    var link: String
+}
+
 /// Register your application's routes here.
 ///
 /// [Learn More →](https://docs.vapor.codes/3.0/getting-started/structure/#routesswift)
@@ -15,20 +20,23 @@ public func routes(_ router: Router) throws {
     }
 
     router.get("posts") { req -> Future<View> in
-        let context: [String: [String]] = ["posts": allPosts]
+        var posts = [Post]()
+        for (spaced, dashed) in allPostsZipped {
+            let post = Post(title: spaced, link: dashed)
+            posts.append(post)
+        }
         let leaf = try req.make(LeafRenderer.self)
-        return leaf.render("posts", context)
+        return leaf.render("posts", ["posts": posts])
     }
 
     router.get("posts", String.parameter) { req -> Future<View> in
         let param = try req.parameters.next(String.self)
-        let unescapedParam = param.removingPercentEncoding! // TODO: Fix this
-        let postPath = postPathByTitle[unescapedParam]! // TODO: Fix this
+        let postPath = postPathByTitle[param]! // TODO: Fix this
         let leaf = try req.make(LeafRenderer.self)
         let dir = workingDir()
         let location = "\(dir)/Posts/\(postPath)"
         let fileContent = try! String(contentsOfFile: location)
-        let context: [String: String] = ["post": try markdownToHTML(fileContent), "title": unescapedParam]
+        let context: [String: String] = ["post": try markdownToHTML(fileContent), "title": param]
         return leaf.render("post", context)
     }
 }
